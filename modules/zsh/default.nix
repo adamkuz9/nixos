6359553755be349222a0503b19c7b219d491add8
Adam <kartofleadama@gmail.com>
2026-03-10 13:28:32 +0100

    NixOS Generation: laptop 57

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -23,9 +23,16 @@ in {
         autosuggestion.enable = true;
         syntaxHighlighting.enable = true;
 
+        plugins = [
+          {
+            name = "powerlevel10k";
+            src = pkgs.zsh-powerlevel10k;
+            file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+          }
+        ];
+
         oh-my-zsh = {
           enable = true;
-          theme = "robbyrussell";
           plugins = [
             "git"
             "sudo"
